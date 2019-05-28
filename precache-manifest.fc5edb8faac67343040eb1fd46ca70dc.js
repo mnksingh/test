3f7f537947e4c34d1df1056ecc9b84a4fbc143bd
mayank_cobold <mayank@cobold.co>
2019-05-29 02:37:01 +0800

    Updates

---
+++ precache-manifest.fc5edb8faac67343040eb1fd46ca70dc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b061be879fb76520d4d80796f38dfc",
+    "url": "/mnksingh/test.git/index.html"
+  },
+  {
+    "revision": "e8c32e642480e80ae1b5",
+    "url": "/mnksingh/test.git/static/js/2.a914a011.chunk.js"
+  },
+  {
+    "revision": "1b2f88ccad7b6f7058ed",
+    "url": "/mnksingh/test.git/static/js/main.6717a8c6.chunk.js"
+  },
+  {
+    "revision": "9fee440793c03b61ac01",
+    "url": "/mnksingh/test.git/static/js/runtime~main.2b513b46.js"
+  }
+]);
